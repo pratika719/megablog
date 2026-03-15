59bc36ed8d2dfea4e68b86d38dda7423683a1a60
pratika719 <pratikagrawal719@gmail.com>
2026-03-15 16:54:19 +0530

    add auth service

---
+++ src/appwrite/auth.js
@@ -0,0 +1,69 @@
+import config from '../config/config.js'
+import { Client, Account } from 'appwrite';
+
+
+export class AuthService{
+    client=new Client()
+    account;
+
+
+    constructor(){
+        this.client.setEndpoint(config.appwriteUrl).setProject(config.appwriteProjectId);
+        this.account = new Account(this.client);
+        
+    }
+
+    async createAccount(email,password,name){
+        try{
+
+          const userAccount=  await this.account.create(ID.unique(), email, password, name); 
+          if (userAccount){
+            return this.login({email,password});
+
+          }
+          else{
+            throw new Error('Failed to create account');
+          }
+
+        } catch(error){
+            throw error;
+        }
+
+}
+
+async login(email,password){
+    try{
+       return  await this.account.createEmailSession(email,password);
+    } catch(error){
+        throw error;
+    }
+}
+
+async getCurrentUser(){
+    try {
+        return await this.account.get();
+        
+    } catch (error) {
+        throw error;
+    }
+
+    return null;
+
+
+}
+
+async logout(){
+    try{
+        await this.account.deleteSession('current');
+    } catch(error){
+        throw error;
+
+    }
+
+}
+}
+
+const authService = new AuthService();
+
+
+export default authService
